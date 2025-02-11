54577184fdb67405dbb51e165f47505927367167
R0rt1z2 <me@r0rt1z2.com>
2025-02-11 14:37:29 +0100

    aine: Set Architecture flags
    Change-Id: Ie0d7a78a9c22fdc7a60d4996ec660d77647ce25d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,5 +6,12 @@
 
 DEVICE_PATH := device/energy/aine
 
+# Architecture
+TARGET_ARCH := arm
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT  := cortex-a7
+
 # Inherit the proprietary files
 -include vendor/energy/aine/BoardConfigVendor.mk
