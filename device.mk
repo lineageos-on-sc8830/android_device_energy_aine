fa340044d743414e9ae2576e10a17931150bcd2b
R0rt1z2 <me@r0rt1z2.com>
2025-02-11 14:53:11 +0100

    aine: Import minimal rootdir
    Change-Id: I074a6923eb05037b75ff6a12153a8e8a452de8ec

--- device.mk
+++ device.mk
@@ -4,6 +4,21 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Rootdir
+PRODUCT_PACKAGES += \
+    fstab.sc8830 \
+    init.board.rc \
+    init.sc8830.rc \
+    init.sc8830.usb.rc \
+    init.storage.rc \
+    ueventd.sc8830.rc
+
+ifneq ($(filter eng userdebug,$(TARGET_BUILD_VARIANT)),)
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    ro.adb.secure=0 \
+    ro.secure=0
+endif
+
 # Inherit the proprietary files
 $(call inherit-product-if-exists, vendor/energy/aine/aine-vendor.mk)
 
